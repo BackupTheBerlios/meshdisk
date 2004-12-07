1cc91b068f2e47c8d0d21da3043b6c488868d1fd
mihi <mihi>
2004-12-07 08:11:22 +0000

    Working on uClibc_toolchain build

--- meshdisk/make/uClibc_toolchain.mk
+++ meshdisk/make/uClibc_toolchain.mk
@@ -1,2 +1,13 @@
 
-$SOURCE=http://
+LOCATION=http://meshdisk.berlios.de/src/
+FILE=uClibc-toolchain-snapshot-20041207.tar.gz
+DIR=toolchain
+
+bin/toolchain_i386:  build/$(DIR)
+
+
+build/$(DIR): sources/$(FILE)
+	cd build && tar -xvzf ../sources/$(FILE)
+
+sources/$(FILE): 
+	cd sources && wget --passive-ftp $(LOCATION)$(FILE)
