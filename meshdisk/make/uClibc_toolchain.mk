85143ba08abdef56ed861d9e60f914598125c6cc
mihi <mihi>
2004-12-07 08:22:45 +0000

    first version of uClibc_toolchain builds

--- meshdisk/make/uClibc_toolchain.mk
+++ meshdisk/make/uClibc_toolchain.mk
@@ -2,9 +2,12 @@
 LOCATION=http://meshdisk.berlios.de/src/
 FILE=uClibc-toolchain-snapshot-20041207.tar.gz
 DIR=toolchain
+TARGET=build/$(DIR)/gcc-3.3.x/toolchain_i386/bin/linux-i386-uclibc-gcc
 
-bin/toolchain_i386:  build/$(DIR)
-
+bin/toolchain_i386:  $(TARGET)
+	
+$(TARGET): build/$(DIR)
+	make -C build/$(DIR)/gcc-3.3.x 
 
 build/$(DIR): sources/$(FILE)
 	cd build && tar -xvzf ../sources/$(FILE)
