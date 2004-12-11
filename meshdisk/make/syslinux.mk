8f3a2f712cc05f5bc187497328b4cc0fa4feb026
mihi <mihi>
2004-12-11 07:07:45 +0000

    syslinux makes and installs now.

--- meshdisk/make/syslinux.mk
+++ meshdisk/make/syslinux.mk
@@ -3,8 +3,22 @@ LOCATION_SL=http://www.kernel.org/pub/linux/utils/boot/syslinux/
 FILE_SL=syslinux-2.11.tar.gz
 DIR_SL=syslinux-2.11
 TARGET_SL=build/$(DIR)/syslinux
+# Things to install in /usr/bin
+SL_INSTALL_BIN   = syslinux gethostip ppmtolss16 lss16toppm
+# Things to install in /usr/lib/syslinux
+SL_INSTALL_AUX   = pxelinux.0 isolinux.bin isolinux-debug.bin \
+                syslinux.com syslinux.exe copybs.com memdisk/memdisk
+# Things to install in /usr/lib
+SL_INSTALL_LIB   = $(LIB_SO) libsyslinux.a
+
 
 bin.tmp/syslinux:  $(TARGET_SL)
+	mkdir -p bin.tmp/syslinux/bin
+	mkdir -p bin.tmp/syslinux/lib
+	mkdir -p bin.tmp/syslinux/lib/syslinux
+	cd build/$(DIR_SL) && cp $(SL_INSTALL_BIN) ../../bin.tmp/syslinux/bin
+	cd build/$(DIR_SL) && cp $(SL_INSTALL_AUX) ../../bin.tmp/syslinux/lib/syslinux
+	cd build/$(DIR_SL) && cp $(SL_INSTALL_LIB) ../../bin.tmp/syslinux/lib
 	
 $(TARGET_SL): build/$(DIR_SL)
 	make -C build/$(DIR_SL) installer
